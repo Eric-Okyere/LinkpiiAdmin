1a57340eef6930c8254fbdecec22228c5881ea77
Eric-Okyere <ericokyere018@gmail.com>
2024-09-04 13:36:15 +0000

    Services

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';  // Import Link from react-router-dom
 import { Card } from 'flowbite-react';
 import { BeatLoader } from 'react-spinners';
