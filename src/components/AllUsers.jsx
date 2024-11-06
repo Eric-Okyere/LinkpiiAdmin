2bdf2f83d3667734cb4296f4e229890e0f02de41
Eric-Okyere <ericokyere018@gmail.com>
2024-11-06 13:14:45 +0000

    Whatsapp comment

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -164,7 +164,7 @@ const AllUsers = () => {
     const internationalPhone = `${formattedPhone}`; // No '+' attached
   
     console.log('International Phone:', internationalPhone);
-  const message = encodeURIComponent("Welcome to Linkpii! It helps you post video and pictures of your work, products or shop. You can also order a KIA driver by negotiation. It helps you rent a room, book a hotel or buy estate. Our main aim is to promote agrictulture. ");
+  const message = encodeURIComponent("Welcome to Linkpii! It helps you post video and pictures of your work, products or shop. You can also order a KIA driver by negotiation to convey your products. It helps you rent a room, book a hotel or buy an estate. Our main aim is to promote agrictulture.");
     const whatsappUrl = `https://wa.me/${internationalPhone}?text=${message}`;
   
     // Open WhatsApp in a new tab
