11e6850466a1149351e99ad9b5e9feca3d37ab7b
Eric-Okyere <ericokyere018@gmail.com>
2025-01-01 11:14:43 +0000

    Message

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -158,7 +158,7 @@ const AllUsers = () => {
     formattedPhone = formattedPhone.startsWith('+') ? formattedPhone.slice(1) : formattedPhone;
     const internationalPhone = `${formattedPhone}`;
     const message = encodeURIComponent(
-      "Welcome to Linkpii! It helps you post video and pictures of your work, products or shop. You can also order a KIA driver by negotiation to convey your products. It helps you rent a room, book a hotel or buy an estate. Our main aim is to promote agrictulture."
+      "Welcome to Linkpii! It helps you post video and pictures of your work, products or shop. You can also order a KIA driver by negotiation to convey your products. It helps you rent a room, book a hotel or buy an estate. Our main aim is to promote agrictulture. Can we know what you want to buy, sell or services you provide?"
     );
     const whatsappUrl = `https://wa.me/${internationalPhone}?text=${message}`;
     console.log(internationalPhone)
@@ -176,7 +176,7 @@ const AllUsers = () => {
     }
   
     const message = encodeURIComponent(
-      "Welcome to Linkpii! It helps you post video and pictures of your work, products or shop. You can also order a KIA driver by negotiation to convey your products. It helps you rent a room, book a hotel or buy an estate. Our main aim is to promote agrictulture."
+      "Welcome to Linkpii! It helps you post video and pictures of your work, products or shop. You can also order a KIA driver by negotiation to convey your products. It helps you rent a room, book a hotel or buy an estate. Our main aim is to promote agrictulture. Can we know what you want to buy, sell or services you provide?"
     );
   
     // Open the default SMS app
